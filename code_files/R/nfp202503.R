8f0a03489155bb652caec2b286a80b13a6a3cbed
David T. Flynn <73048403+flynnecon@users.noreply.github.com>
2025-04-01 01:06:40 -0500

    Update nfp202503.R

--- code_files/R/nfp202503.R
+++ code_files/R/nfp202503.R
@@ -29,20 +29,20 @@ urlfile="https://raw.githubusercontent.com/flynnecon/forecasting2025/main/nfp-20
 ```
 #Getting the data read into R and then made into our time series analysis format. Note we do not need a training or test data division with the fact we are looking at a forecast 1 period ahead.
 ```{r Employment data work}
-empdata202402 <- read_csv(url(urlfile))
-empdata202402 <- empdata202402[-c(1)]
-empdata202402$date <- seq(as.Date("2014-01-01"), as.Date("2024-01-01"), by="1 month")
-emp_tsbl202402 <- empdata202402 |> mutate(month=yearmonth(date)) |> as_tsibble(index=month)
+empdata202502 <- read_csv(url(urlfile))
+empdata202502 <- empdata202402[c(2)]
+empdata202502$date <- seq(as.Date("2001-01-01"), as.Date("2025-02-01"), by="1 month")
+emp_tsbl202502 <- empdata202502 |> mutate(month=yearmonth(date)) |> as_tsibble(index=month)
 ```
 #Just plotting employment here, the not seasoanlly adjusted biut. 
 ```{r plot of employment}
-emp_tsbl202402 |> ggplot(aes(x=month,y=emplnsa)) + geom_line()
+emp_tsbl202502 |> ggplot(aes(x=month,y=nfp)) + geom_line()
 ```
 #Looking at the time series decomposition.
 ```{r decomposition work}
-emp_tsbl202402 |>
+emp_tsbl202502 |>
   model(
-    STL(emplnsa ~ trend(window = 7) +
+    STL(nfp ~ trend(window = 7) +
                    season(window = "periodic"),
     robust = TRUE)) |>
   components() |>
@@ -51,14 +51,14 @@ emp_tsbl202402 |>
 
 #Take the decomposition model and then a naive forecast will see you through to the outcome. 
 ```{r forecast by decomposition}
-fit_dcmp <- emp_tsbl202402 |>
+fit_dcmp <- emp_tsbl202502 |>
   model(stlf = decomposition_model(
-    STL(emplnsa ~ trend(window = 7), robust = TRUE),
+    STL(nfp ~ trend(window = 7), robust = TRUE),
     NAIVE(season_adjust)
   ))
 fit_dcmp |>
   forecast() |>
-  autoplot(emp_tsbl202402)+
+  autoplot(emp_tsbl202502)+
   labs(y = "Number of people",
        title = "US employment")
 fit_out <-fit_dcmp |>
